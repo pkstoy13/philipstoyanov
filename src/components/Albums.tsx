b90c46a88fafce2c2b901f8831a1eb28d90bebb3
pkstoy13 <pkstoyanov1@gmail.com>
2024-06-24 17:58:29 -0400

    albums are in and properly sized, still need to fix small lg screen bug

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -1,19 +1,29 @@
+import Igor from "../assets/album art/igor.jpg";
+import Rodeo from "../assets/album art/rodeo.jpg";
+import CFM from "../assets/album art/care for me.jpg";
+import Blonde from "../assets/album art/blonde.jpeg";
+import Damn from "../assets/album art/damn..png";
+import BR from "../assets/album art/br resized.png";
+import ALLA from "../assets/album art/alla resized.png";
+import Into from "../assets/album art/into resized.png";
+import LD from "../assets/album art/ld resized.png";
+
 export function Albums() {
   return (
     <>
-      <h1 className="flex cursor-default border-white border-b-2 mb-2 mt-4 mx-20 lg:mx-32">
+      <h1 className="cursor-default border-white border-b-2 mb-2 mt-4 mx-20 lg:mx-44">
         Albums
       </h1>
-      <div className="grid grid-flow-row md:gap-8 md:grid-cols-3 md:grid-rows-2 mx-20 lg:mx-32">
-        <div className="project">Igor</div>
-        <div className="project">At Long Last A$AP</div>
-        <div className="project">Rodeo</div>
-        <div className="project">Into</div>
-        <div className="project">CARE FOR ME</div>
-        <div className="project">Blonde</div>
-        <div className="project">DAMN.</div>
-        <div className="project">Love Deluxe</div>
-        <div className="project">Backstage Raver</div>
+      <div className="grid grid-flow-row md:gap-8 sm:grid-cols-2 md:grid-cols-3 md:grid-rows-3 gap-4 mx-20 lg:mx-44">
+        <img className="project" src={Igor} alt="Igor" />
+        <img className="project" src={ALLA} alt="ALLA" />
+        <img className="project" src={Rodeo} alt="Rodeo" />
+        <img className="project" src={Into} alt="Into" />
+        <img className="project" src={CFM} alt="Care For Me" />
+        <img className="project" src={Blonde} alt="Blonde" />
+        <img className="project" src={Damn} alt="DAMN." />
+        <img className="project" src={LD} alt="love deluxe" />
+        <img className="project" src={BR} alt="Backstage Raver" />
       </div>
     </>
   );
