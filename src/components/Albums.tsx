a648fd192573c2d1290ed6997beba85f97e4d2d7
pkstoy13 <pkstoyanov1@gmail.com>
2024-06-26 20:02:58 -0400

    album modals have been started, still need refinement

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -9,13 +9,40 @@ import Into from "../assets/album art/into resized.png";
 import LD from "../assets/album art/ld resized.png";
 
 export function Albums() {
+  /*const igor = document
+    .getElementById("my_modal_1")!
+    .showModal() as HTMLButtonElement;
+*/
   return (
     <>
       <h1 className="cursor-default border-white border-b-2 mb-2 mt-4 mx-20 lg:mx-44">
         Albums
       </h1>
       <div className="grid grid-flow-row md:gap-8 sm:grid-cols-2 md:grid-cols-3 md:grid-rows-3 gap-4 mx-20 lg:mx-44">
-        <img className="project" src={Igor} alt="Igor" />
+        <img
+          className="project"
+          onClick={() => {
+            const modal = document.getElementById(
+              "my_modal_1"
+            ) as HTMLDialogElement;
+            modal.showModal();
+          }}
+          src={Igor}
+          alt="Igor"
+        />
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box max-w-5xl h-3/4 bg-indigo-600 flex space-x-2">
+            <img
+              src={Igor}
+              className="shadow-xl rounded-xl shadow-indigo-900"
+              alt="Igor"
+            />
+            <p className="inline">Igor - Tyler, The Creator</p>
+          </div>
+          <form method="dialog" className="modal-backdrop">
+            <button />
+          </form>
+        </dialog>
         <img className="project" src={ALLA} alt="ALLA" />
         <img className="project" src={Rodeo} alt="Rodeo" />
         <img className="project" src={Into} alt="Into" />
